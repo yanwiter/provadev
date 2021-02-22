d08a37dbcf3af67d76d3f366a168719bfdc5fc3e
unknown <Castgroup@BHZNOT060.castmeta.local>
2021-02-22 18:13:00 -0300

    Primeiro commit

---
+++ src/main/java/com/provadevjava/provadev/ProvadevApplication.java
@@ -0,0 +1,13 @@
+package com.provadevjava.provadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvadevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/provadevjava/provadev/ProvadevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provadevjava.provadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvadevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
